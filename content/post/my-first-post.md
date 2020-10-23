50af544cc3c74fb87c7a4c1084dcc1baa666b1b4
xingyys <598223084@qq.com>
2020-10-23 13:47:00 +0800

    init

---
+++ content/post/my-first-post.md
@@ -0,0 +1,6 @@
+---
+title: "My First Post"
+date: 2020-10-23T11:22:16+08:00
+draft: true
+---
+
---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo -t even 
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+git checkout --orphan gh-pages
+git reset --hard
+git commit --allow-empty -m $msg
+git push upstream gh-pages
+git checkout master
